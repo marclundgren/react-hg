a9e3e7c8d38801f694b73331730deb2edb8281de
Marc Lundgren <marclundgren2.0@gmail.com>
2020-01-12 13:27:10 -0800

    make this a npm module

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import styled from "styled-components";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Hg = styled.div`
+  height: 100vh;
+  display: grid;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  grid-template-areas:
+    "header header header"
+    "navigation main aside"
+    "footer footer footer";
+  grid-template-columns: 200px 1fr 200px;
+  grid-template-rows: auto 1fr auto;
+  grid-gap: 10px;
+
+  // Responsive
+  @media (max-width: 768px) {
+    grid-template-areas:
+      "header"
+      "navigation"
+      "main"
+      "aside"
+      "footer";
+
+    grid-template-columns: 1fr;
+    grid-template-rows:
+      auto /* Header */
+      minmax(75px, auto) /* Nav */
+      1fr /* Main */
+      minmax(75px, auto) /* Aside */
+      auto; /* Footer */
+  }
+`;
+
+export const Header = styled.header`
+  grid-area: header;
+`;
+export const Navigation = styled.nav`
+  grid-area: navigation;
+`;
+export const Main = styled.main`
+  grid-area: main;
+`;
+export const Footer = styled.footer`
+  grid-area: footer;
+`;
+export const Aside = styled.aside`
+  grid-area: aside;
+`;
+
+export default Hg;
